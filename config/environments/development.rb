6394d6200d202e1d273c50f9811ad74ff37d90b9
Ricardo <universius@gmail.com>
2017-05-03 21:43:16 -0300

    bootstrap

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -45,6 +45,11 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  # Show logs in console.
+ logger           = ActiveSupport::Logger.new(STDOUT)
+ logger.formatter = config.log_formatter
+ config.logger = ActiveSupport::TaggedLogging.new(logger)
+
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
